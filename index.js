a3d8b7e4c8f388f13718e8cfd9a8b04bf6cdfdef
yenhoang <japocoin@gmail.com>
2021-05-08 00:03:14 +0200

    eventlistener to disable and pic added

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const $app = document.getElementById("app");
 const Avatar = (param) => {
   const src=`https://randomuser.me/api/portraits/women/${param.id}.jpg`;
-  return `<picture> <img src="${src}" /> ${param.name} </picture> `;
+  return `<picture> <img src="${src}" /> <strong>${param.name} </string></picture> `;
 }
 
 $app.innerHTML += Avatar({id: 11, name: "Jane"})
 $app.innerHTML += Avatar({id: 5, name: "Yasmine"})
+
+$app.querySelectorAll('img').forEach(img => {
+  img.addEventListener('click', () => {
+    img.classList.toggle('disabled')
+  })
+})
